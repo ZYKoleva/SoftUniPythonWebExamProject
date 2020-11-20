cbbd777a6d9b8faccfd8f8da5790d12b3c728d6d
ZYKoleva <zykoleva@gmail.com>
2020-11-20 18:48:05 +0200

    added templates and style
    templates about_us, general_rules, login and register added and respective style sheets added

--- estate_app/urls.py
+++ estate_app/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 
-from estate_app.views import load_home_page
+from estate_app.views import load_home_page, about_us, general_rules, register, login
 
 urlpatterns = [
-    path('', load_home_page, name = 'load home page')
+    path('', load_home_page, name = 'load home page'),
+    path('about/', about_us, name='load about us page'),
+    path('general_rules/', general_rules, name='load general rules'),
+    path('register/', register, name='load register page'),
+    path('login/', login, name='load login page'),
 ]
--- estate_app/views.py
+++ estate_app/views.py
@@ -3,3 +3,19 @@ from django.shortcuts import render
 # Create your views here.
 def load_home_page(request):
     return render(request, 'home_page.html')
+
+
+def about_us(request):
+    return render(request, 'about_us.html')
+
+
+def general_rules(request):
+    return render(request, 'general_rules.html')
+
+
+def register(request):
+    return render(request, 'register.html')
+
+
+def login(request):
+    return render(request, 'login.html')
